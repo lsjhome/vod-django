c2b627cd06ce07fa76abd1bd5829a0ae3bc5e462
lsjhome007 <lsjhome007@gmail.com>
2018-04-26 17:19:09 +0900

    fixed untracked files, dojo.views.mysum 에서 x/y/z 인자 3개를 처리

--- dojo/views.py
+++ dojo/views.py
@@ -1,3 +1,10 @@
+# dojo/views.py
+from django.http import HttpResponse
 from django.shortcuts import render
+
+
+def mysum(request, x, y=0, z=0):
+    # request: HttpRequest
+    return HttpResponse(int(x) + int(y) + int(z))
 
 # Create your views here.
