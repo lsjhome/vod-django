b28cf05b252f579fdda885fe1afd4c39c431d33a
lsjhome007 <lsjhome007@gmail.com>
2018-04-26 17:38:30 +0900

    dojo.views.hello : 한글 문자열 매칭

--- dojo/urls.py
+++ dojo/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-        url(r'^sum/(?P<numbers>[\d/]+)/$', views.mysum)
+        url(r'^sum/(?P<numbers>[\d/]+)/$', views.mysum),
+        url(r'^hello/(?P<name>[ㄱ-힣]+)/(?P<age>\d+)/$', views.hello)
         ]
--- dojo/views.py
+++ dojo/views.py
@@ -8,4 +8,5 @@ def mysum(request, numbers):
     result = sum(map(lambda s: int(s or 0), numbers.split("/")))
     return HttpResponse(result)
 
-# Create your views here.
+def hello(request, name, age):
+    return HttpResponse('안녕하세요. {}. {}살이시네요'.format(name, age))
