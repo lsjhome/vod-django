67b711bf46dc8e31d6359f5090a7e83b1da9ef78
lsjhome007 <lsjhome007@gmail.com>
2018-04-26 19:52:54 +0900

    함수 기반 뷰 (FBV)의 4가지 패턴

--- dojo/views.py
+++ dojo/views.py
@@ -1,5 +1,8 @@
 # dojo/views.py
-from django.http import HttpResponse
+import os
+
+from django.conf import settings
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 
 
@@ -8,5 +11,39 @@ def mysum(request, numbers):
     result = sum(map(lambda s: int(s or 0), numbers.split("/")))
     return HttpResponse(result)
 
+
 def hello(request, name, age):
     return HttpResponse('안녕하세요. {}. {}살이시네요'.format(name, age))
+
+
+def post_list1(request):
+    name = '공유'
+    return HttpResponse('''
+            <h1>AskDjango</h1>
+            <p>{name}</p>
+            <p>여러분의 파이썬&장고 페이스메이커가 되어드리겠습니다.</p>
+            '''.format(name=name))
+
+
+def post_list2(request):
+    name = '공유'
+    return render(request, 'dojo/post_list.html', {'name':name})
+
+
+def post_list3(request):
+    return JsonResponse({
+        'message': '안녕 파이썬&장고',
+        'items': ['파이썬', '장고', 'Celery', 'Azure', 'AWS'],
+        }, json_dumps_params={'ensure_ascii': False})
+
+
+def excel_download(request):
+    #filepath = '/Users/smfc/study_project/ask-django-basic/vod-django/gdplev.xlsx'
+    filepath = os.path.join(settings.BASE_DIR, 'gdplev.xlsx')
+    filename = os.path.basename(filepath)
+    with open(filepath, 'rb') as f:
+        response = HttpResponse(f, content_type='application/vnd.ms-excel') # text/html
+        response['Content-Disposition'] = 'attachment; filename="{}"'.format(filename)
+        return response
+
+
